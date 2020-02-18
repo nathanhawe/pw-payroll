bfcd13433f89352b4b255fafe59fd8bf7b7f4005
Nathan Hawe <nathanhawe@gmail.com>
2020-02-18 14:47:12 -0800

    Add constants for Company.

---
+++ Payroll.Domain/Constants/Company.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payroll.Domain.Constants
+{
+    public static class Company
+    {
+        public const string Plants = "P";
+        public const string Ranches = "R";
+    }
+}
