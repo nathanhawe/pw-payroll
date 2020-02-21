8ea5784dd458130477b842871759ffa25846c230
Nathan Hawe <nathanhawe@gmail.com>
2020-02-21 14:08:10 -0800

    Add ranch hourly rate selector tests.

---
+++ Payroll.Service/RanchHourlyRateSelector.cs
@@ -0,0 +1,21 @@
+﻿using Payroll.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payroll.Service
+{
+    public class RanchHourlyRateSelector
+    {
+        private ICrewLaborWageSelector _crewLaborWageSelector;
+        public RanchHourlyRateSelector(ICrewLaborWageSelector crewLaborWageSelector)
+        {
+            _crewLaborWageSelector = crewLaborWageSelector ?? throw new ArgumentNullException(nameof(crewLaborWageSelector));
+        }
+
+        public decimal GetRanchHourlyRate(string payType, int crew, int laborCode, decimal employeeHourlyRate, decimal hourlyRateOverride)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Payroll.UnitTest/Mocks/MockCrewLaborWageSelector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payroll.UnitTest.Mocks
+{
+    /// <summary>
+    /// Simple mock of the ICrewLaborWageSelector interface that returns a single value for all dates.
+    /// </summary>
+    public class MockCrewLaborWageSelector : Payroll.Service.Interface.ICrewLaborWageSelector
+    {
+        private decimal _defaultWage;
+
+        public MockCrewLaborWageSelector(decimal defaultWage)
+        {
+            _defaultWage = defaultWage;
+        }
+
+        public decimal GetCrewLaborWage(DateTime shiftDate)
+        {
+            return _defaultWage;
+        }
+    }
+}
