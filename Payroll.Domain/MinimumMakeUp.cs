0012912b519c263625e002c669da1c3b42da286b
Nathan Hawe <nathanhawe@gmail.com>
2020-02-26 16:34:26 -0800

    Add ranch minimum make up calculator tests.

---
+++ Payroll.Domain/MinimumMakeUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payroll.Domain
+{
+    public class MinimumMakeUp
+    {
+        public string EmployeeId { get; set; }
+        public int Crew { get; set; }
+        public DateTime WeekEndDate { get; set; }
+        public DateTime ShiftDate { get; set; }
+        public decimal Gross { get; set; }
+    }
+}
---
+++ Payroll.Service/RanchMinimumMakeUpCalculator.cs
@@ -0,0 +1,15 @@
+﻿using Payroll.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payroll.Service
+{
+    public class RanchMinimumMakeUpCalculator
+    {
+        public List<MinimumMakeUp> GetMinimumMakeUps(List<WeeklySummary> weeklySummaries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
