7779d5bc450818e644291216e1546d067c92db28
Nathan Hawe <nathanhawe@gmail.com>
2020-02-28 13:48:44 -0800

    wip Check in control service as is.

--- Payroll.Service/TimeAndAttendanceService.cs
+++ Payroll.Service/TimeAndAttendanceService.cs
@@ -195,7 +195,16 @@ namespace Payroll.Service
             // Minimum Make Up is made by comparing the effective rate against minimum wage.  If minimum wage is greater than the effective rate, the difference
             // should be used to create a minimum make up line and the higher of the two rates is used for OT, DT, etc.  When a week has multiple minimum wages, the
             // the effective rate calculated against the higher minimum wage should be used as the weekly effective rate.
-            var minimumMakeUps = _minimumMakeUpCalculator.GetMinimumMakeUps(weeklySummaries);
+            var minimumMakeUpRecords = _minimumMakeUpCalculator.GetMinimumMakeUps(weeklySummaries).Select(x => new RanchPayLine
+            {
+                EmployeeId = x.EmployeeId,
+                WeekEndDate = x.WeekEndDate,
+                ShiftDate = x.ShiftDate,
+                PayType = PayType.MinimumAssurance,
+                OtherGross = x.Gross
+            }).ToList();
+            _totalGrossCalculator.CalculateTotalGross(minimumMakeUpRecords);
+            _context.AddRange(minimumMakeUpRecords);
 
             /* WOT Hours */
             var weeklyOt = _weeklyOverTimeHoursCalculator.GetWeeklyOTHours(weeklySummaries);
@@ -212,6 +221,7 @@ namespace Payroll.Service
             }).ToList();
             _grossFromHoursCalculator.CalculateGrossFromHours(overTimeRecords);
             _totalGrossCalculator.CalculateTotalGross(overTimeRecords);
+            _context.AddRange(overTimeRecords);
 
             var doubleTimeRecords = dailySummaries.Where(x => x.DoubleTimeHours > 0).Select(x => new RanchPayLine
             {
@@ -224,6 +234,7 @@ namespace Payroll.Service
             }).ToList();
             _grossFromHoursCalculator.CalculateGrossFromHours(doubleTimeRecords);
             _totalGrossCalculator.CalculateTotalGross(doubleTimeRecords);
+            _context.AddRange(doubleTimeRecords);
 
             /* WOT Gross (Requires effective weekly rate) */
             var weeklyOverTimeRecords = weeklyOt.Where(x => x.OverTimeHours > 0).Select(x => new RanchPayLine
@@ -236,11 +247,217 @@ namespace Payroll.Service
             }).ToList();
             _grossFromHoursCalculator.CalculateGrossFromHours(weeklyOverTimeRecords);
             _totalGrossCalculator.CalculateTotalGross(weeklyOverTimeRecords);
+            _context.AddRange(weeklyOverTimeRecords);
+            
 
             /* Update Reporting Pay / Comp Time hourly rates (Requires effective weekly rate) */
-
+            var reportingPayRecords = _context.RanchPayLines.Where(x => x.BatchId == batchId && (x.PayType == PayType.CompTime || x.PayType == PayType.ReportingPay)).ToList();
+            foreach(var record in reportingPayRecords)
+            {
+                // Needs to select Max from effective and minimum wage.
+                //record.HourlyRateOverride = weeklySummaries
+                //    .Where(x => x.EmployeeId == record.EmployeeId && x.WeekEndDate == record.WeekEndDate)
+                //    .OrderByDescending(x => x.EffectiveHourlyRate)
+                //    .FirstOrDefault()
+                //    ?.EffectiveHourlyRate
+                //    ?? 0;
+            }
+            _grossFromHoursCalculator.CalculateGrossFromHours(reportingPayRecords);
+            _totalGrossCalculator.CalculateTotalGross(reportingPayRecords);
 
             /* Update Non-Productive Time hourly rates (Requires effective weekly rate) */
+            var nonProductiveRecords = _context.RanchPayLines.Where(x => x.BatchId == batchId && (x.LaborCode == 380 || x.LaborCode == 381)).ToList();
+            foreach (var record in nonProductiveRecords)
+            {
+                // Needs to select Max from effective and minimum wage.
+                //record.HourlyRateOverride = weeklySummaries
+                //    .Where(x => x.EmployeeId == record.EmployeeId && x.WeekEndDate == record.WeekEndDate)
+                //    .OrderByDescending(x => x.EffectiveHourlyRate)
+                //    .FirstOrDefault()
+                //    ?.EffectiveHourlyRate
+                //    ?? 0;
+            }
+            _grossFromHoursCalculator.CalculateGrossFromHours(nonProductiveRecords);
+            _totalGrossCalculator.CalculateTotalGross(nonProductiveRecords);
+
+            _context.SaveChanges();
+
+
+            /* Calculate Adjustments */
+            CalculateAdjustments(batchId, company);
+
+            /* Create Summaries */
+        }
+
+        private void CalculateAdjustments(int batchId, string company)
+        {
+            DateTime weekendOfAdjustmentPaid = _context.RanchAdjustmentLines.Where(x => x.BatchId == batchId).OrderByDescending(x => x.WeekEndOfAdjustmentPaid).FirstOrDefault()?.WeekEndOfAdjustmentPaid ?? new DateTime(2000, 1, 1);
+            
+            /* Gross Calculations */
+            // Hourly
+            // Include sick leave to be calculated as we expect to be provided an "Old Hourly Rate" instead of figuring
+            // out the rate ourselves.
+            var hourlyLines = _context.RanchAdjustmentLines.Where(x => x.BatchId == batchId && !x.IsOriginal &&
+                (
+                    x.PayType == PayType.Regular
+                    || x.PayType == PayType.HourlyPlusPieces
+                    || x.PayType == PayType.SpecialAdjustment
+                    || x.PayType == PayType.ReportingPay
+                    || x.PayType == PayType.CompTime
+                    || x.PayType == PayType.Vacation
+                    || x.PayType == PayType.Holiday
+                    || x.PayType == PayType.SickLeave))
+                .ToList();
+            _grossFromHoursCalculator.CalculateGrossFromHours(hourlyLines);
+            _context.UpdateRange(hourlyLines);
+            _context.SaveChanges();
+
+            // Pieces
+            var pieceLines = _context.RanchAdjustmentLines.Where(x => x.BatchId == batchId && !x.IsOriginal &&
+                (
+                    x.PayType == PayType.Pieces
+                    || x.PayType == PayType.HourlyPlusPieces))
+                .ToList();
+            _grossFromPiecesCalculator.CalculateGrossFromPieces(pieceLines);
+            _context.UpdateRange(pieceLines);
+            _context.SaveChanges();
+
+            // Total
+            var payLines = _context.RanchAdjustmentLines.Where(x => x.BatchId == batchId && !x.IsOriginal).ToList();
+            _totalGrossCalculator.CalculateTotalGross(payLines);
+            _context.UpdateRange(payLines);
+            _context.SaveChanges();
+
+           
+            /* OT/DT/Seventh Day Hours */
+            // Daily summaries group all of the ranch pay lines by Employee, Week End Date, Shift Date, Alternative Work Week, and Minimum Wage.
+            // Additionally it selects the last of Crew and last of FiveEight sorting - I believe - on the Quick Base Record ID.
+            // This needs to be double checked before going to production and the actual rules for the calculation should be confirmed.
+            // The effective daily rate is not used in ranches but is used in plants for the purposes of minimum make up.
+            var dailySummaries = _dailySummaryCalculator.GetDailySummariesFromAdjustments(batchId);
+
+            // Calculate OT/DT/7th Day Hours
+            // This uses the information in the daily summary to correctly calculate how many hours are over time and double time if any.
+            _dailyOverTimeHoursCalculator.SetDailyOTDTHours(dailySummaries);
+
+            // Create Weekly Summaries groups all of the daily summaries by Employee, Week End Date, and Minimum Wage and summarizes the
+            // different types of hours for the week.  This information is used to figure out the effective hourly rate and create minimum
+            // assurance lines.
+            var weeklySummaries = _weeklySummaryCalculator.GetWeeklySummary(dailySummaries);
+
+            // Minimum Make Up is made by comparing the effective rate against minimum wage.  If minimum wage is greater than the effective rate, the difference
+            // should be used to create a minimum make up line and the higher of the two rates is used for OT, DT, etc.  When a week has multiple minimum wages, the
+            // the effective rate calculated against the higher minimum wage should be used as the weekly effective rate.
+            var minimumMakeUpRecords = _minimumMakeUpCalculator.GetMinimumMakeUps(weeklySummaries).Select(x => new RanchPayLine
+            {
+                EmployeeId = x.EmployeeId,
+                WeekEndDate = x.WeekEndDate,
+                ShiftDate = x.ShiftDate,
+                PayType = PayType.MinimumAssurance,
+                OtherGross = x.Gross
+            }).ToList();
+            _totalGrossCalculator.CalculateTotalGross(minimumMakeUpRecords);
+            _context.AddRange(minimumMakeUpRecords);
+
+            /* WOT Hours */
+            var weeklyOt = _weeklyOverTimeHoursCalculator.GetWeeklyOTHours(weeklySummaries);
+
+            /* OT/DT Gross (Requires effective weekly rate) */
+            var overTimeRecords = dailySummaries.Where(x => x.OverTimeHours > 0).Select(x => new RanchPayLine
+            {
+                EmployeeId = x.EmployeeId,
+                WeekEndDate = x.WeekEndDate,
+                ShiftDate = x.ShiftDate,
+                PayType = PayType.OverTime,
+                HoursWorked = x.OverTimeHours,
+                //HourlyRateOverride = Math.Max(x.EffectiveHourlyRate, x.MinimumWage)
+            }).ToList();
+            _grossFromHoursCalculator.CalculateGrossFromHours(overTimeRecords);
+            _totalGrossCalculator.CalculateTotalGross(overTimeRecords);
+            _context.AddRange(overTimeRecords);
+
+            var doubleTimeRecords = dailySummaries.Where(x => x.DoubleTimeHours > 0).Select(x => new RanchPayLine
+            {
+                EmployeeId = x.EmployeeId,
+                WeekEndDate = x.WeekEndDate,
+                ShiftDate = x.ShiftDate,
+                PayType = PayType.DoubleTime,
+                HoursWorked = x.DoubleTimeHours,
+                //HourlyRateOverride = Math.Max(x.EffectiveHourlyRate, x.MinimumWage)
+            }).ToList();
+            _grossFromHoursCalculator.CalculateGrossFromHours(doubleTimeRecords);
+            _totalGrossCalculator.CalculateTotalGross(doubleTimeRecords);
+            _context.AddRange(doubleTimeRecords);
+
+            /* WOT Gross (Requires effective weekly rate) */
+            var weeklyOverTimeRecords = weeklyOt.Where(x => x.OverTimeHours > 0).Select(x => new RanchPayLine
+            {
+                EmployeeId = x.EmployeeId,
+                WeekEndDate = x.WeekEndDate,
+                ShiftDate = x.WeekEndDate,
+                PayType = PayType.WeeklyOverTime,
+                HoursWorked = x.OverTimeHours
+            }).ToList();
+            _grossFromHoursCalculator.CalculateGrossFromHours(weeklyOverTimeRecords);
+            _totalGrossCalculator.CalculateTotalGross(weeklyOverTimeRecords);
+            _context.AddRange(weeklyOverTimeRecords);
+
+
+            /* Update Reporting Pay / Comp Time hourly rates (Requires effective weekly rate) */
+            var reportingPayRecords = _context.RanchAdjustmentLines.Where(x => x.BatchId == batchId && !x.IsOriginal && (x.PayType == PayType.CompTime || x.PayType == PayType.ReportingPay)).ToList();
+            foreach (var record in reportingPayRecords)
+            {
+                // Needs to select Max from effective and minimum wage.
+                //record.HourlyRateOverride = weeklySummaries
+                //    .Where(x => x.EmployeeId == record.EmployeeId && x.WeekEndDate == record.WeekEndDate)
+                //    .OrderByDescending(x => x.EffectiveHourlyRate)
+                //    .FirstOrDefault()
+                //    ?.EffectiveHourlyRate
+                //    ?? 0;
+            }
+            _grossFromHoursCalculator.CalculateGrossFromHours(reportingPayRecords);
+            _totalGrossCalculator.CalculateTotalGross(reportingPayRecords);
+
+            /* Update Non-Productive Time hourly rates (Requires effective weekly rate) */
+            var nonProductiveRecords = _context.RanchAdjustmentLines.Where(x => x.BatchId == batchId && !x.IsOriginal && (x.LaborCode == 380 || x.LaborCode == 381)).ToList();
+            foreach (var record in nonProductiveRecords)
+            {
+                // Needs to select Max from effective and minimum wage.
+                //record.HourlyRateOverride = weeklySummaries
+                //    .Where(x => x.EmployeeId == record.EmployeeId && x.WeekEndDate == record.WeekEndDate)
+                //    .OrderByDescending(x => x.EffectiveHourlyRate)
+                //    .FirstOrDefault()
+                //    ?.EffectiveHourlyRate
+                //    ?? 0;
+            }
+            _grossFromHoursCalculator.CalculateGrossFromHours(nonProductiveRecords);
+            _totalGrossCalculator.CalculateTotalGross(nonProductiveRecords);
+
+            _context.SaveChanges();
+
+
+            /* Figure out total Adjustments */
+            /* Create new Ranch Payroll Lines for all adjustments */
+            var adjustmentLines = _context.RanchAdjustmentLines
+                .Where(x => x.BatchId == batchId)
+                .GroupBy(x => new { x.EmployeeId, x.WeekEndDate }, (key, group) => new
+                {
+                    EmployeeId = key.EmployeeId,
+                    WeekEndDate = key.WeekEndDate,
+                    Gross = group.Select(g => g.TotalGross * (g.IsOriginal ? -1 : 1)).Sum()
+                })
+                .Select(x => new RanchPayLine
+                {
+                    EmployeeId = x.EmployeeId,
+                    WeekEndDate = x.WeekEndDate,
+                    ShiftDate = x.WeekEndDate,
+                    PayType = PayType.Adjustment,
+                    OtherGross = x.Gross,
+                    TotalGross = x.Gross
+                })
+                .ToList();
+            _context.AddRange(adjustmentLines);
+            _context.SaveChanges();
         }
     }
 }
