5711c142154887213b71623a378154b150536187
Nathan Hawe <nathanhawe@gmail.com>
2020-03-18 16:02:15 -0700

    Implement plant daily over time and double time hours calculations. - Implement calculations - Update tests

--- Payroll.Service/PlantDailyOTDTHoursCalculator.cs
+++ Payroll.Service/PlantDailyOTDTHoursCalculator.cs
@@ -2,15 +2,102 @@
 using Payroll.Service.Interface;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Payroll.Service
 {
-    public class PlantDailyOTDTHoursCalculator : IDailyOTDTHoursCalculator
-    {
-        public void SetDailyOTDTHours(List<DailySummary> dailySummaries)
-        {
-            throw new NotImplementedException();
-        }
-    }
+	/// <summary>
+	/// Performs over time and double time hours calculation for plant pay and adjustment lines.
+	/// </summary>
+	public class PlantDailyOTDTHoursCalculator : IDailyOTDTHoursCalculator
+	{
+		public decimal RegularOverTimeThreshold { get; } = 8;
+		public decimal RegularDoubleTimeThreshold { get; } = 12;
+		public decimal AlternativeOverTimeThreshold { get; } = 10;
+		public decimal AlternativeDoubleTimeThreshold { get; } = 12;
+
+		/// <summary>
+		/// Sets the values of <c>OverTimeHours</c> and <c>DoubleTimeHours</c> for the provided <c>DailySummary</c> objects.
+		/// Over time and double time hours are calculated based on the summary's <c>AlternativeWorkWeek</c> flag.
+		/// </summary>
+		/// <param name="dailySummaries"></param>
+		public void SetDailyOTDTHours(List<DailySummary> dailySummaries)
+		{
+			bool calculateSeventhDay;
+
+			foreach (var summary in dailySummaries)
+			{
+				if(summary.ShiftDate.DayOfWeek == DayOfWeek.Sunday)
+				{
+					calculateSeventhDay = HasSevenDays(summary, dailySummaries);
+				}
+				else
+				{
+					calculateSeventhDay = false;
+				}
+
+				if (summary.AlternativeWorkWeek)
+				{
+					SetDailyOTDTHoursWithThresholds(summary, AlternativeOverTimeThreshold, AlternativeDoubleTimeThreshold, calculateSeventhDay);
+				}
+				else
+				{
+					SetDailyOTDTHoursWithThresholds(summary, RegularOverTimeThreshold, RegularDoubleTimeThreshold, calculateSeventhDay); 
+				}
+			}
+		}
+
+		/// <summary>
+		/// Sets the values of <c>DoubleTimeHours</c> and <c>OverTimeHours</c> based on the provided OT and DT thresholds.
+		/// </summary>
+		/// <param name="summary"></param>
+		/// <param name="overTimeThreshold"></param>
+		/// <param name="doubleTimeThreshold"></param>
+		/// <param name="isSeventhDay"></param>
+		private void SetDailyOTDTHoursWithThresholds(DailySummary summary, decimal overTimeThreshold, decimal doubleTimeThreshold, bool isSeventhDay)
+		{
+			// If this employee has worked all seven days and this is the seventh day
+			// then calculate seventh day pay.
+			if(isSeventhDay)
+			{
+				overTimeThreshold = 0;
+				doubleTimeThreshold = 8;
+			}
+
+			if (summary.TotalHours > doubleTimeThreshold)
+			{
+				summary.DoubleTimeHours = summary.TotalHours - doubleTimeThreshold;
+				summary.OverTimeHours = doubleTimeThreshold - overTimeThreshold;
+			}
+			else if (summary.TotalHours > overTimeThreshold)
+			{
+				summary.DoubleTimeHours = 0;
+				summary.OverTimeHours = summary.TotalHours - overTimeThreshold;
+			}
+			else
+			{
+				summary.DoubleTimeHours = summary.OverTimeHours = 0;
+			}
+		}
+
+		/// <summary>
+		/// Returns true if employee in provided <c>DailySummary</c> worked seven days in the same week ending period.
+		/// </summary>
+		/// <param name="summary"></param>
+		/// <param name="dailySummaries"></param>
+		/// <returns></returns>
+		private bool HasSevenDays(DailySummary summary, List<DailySummary> dailySummaries)
+		{
+			var distinctDays = dailySummaries
+				.Where(x =>
+					x.WeekEndDate == summary.WeekEndDate
+					&& x.EmployeeId == summary.EmployeeId
+					&& x.TotalHours > 0)
+				.GroupBy(g => g.ShiftDate)
+				.Count();
+			
+			return (distinctDays == 7);
+		}
+	}
 }
---
+++ Payroll.UnitTest/PlantDailyOTDTHoursCalculatorTests.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payroll.Domain;
+using Payroll.Domain.Constants;
+using Payroll.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Payroll.UnitTest
+{
+	[TestClass]
+	public class PlantDailyOTDTHoursCalculatorTests
+	{
+		private PlantDailyOTDTHoursCalculator _plantDailyOTDTHoursCalculator = new PlantDailyOTDTHoursCalculator();
+
+		[TestMethod]
+		public void RegularSchedule_OverTimeBetweenEightAndTwelveHours()
+		{
+			var dailySummaries = new List<DailySummary>
+			{
+				new DailySummary{ Crew = (int)Plant.Cutler, AlternativeWorkWeek = false, TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Cutler, AlternativeWorkWeek = false, TotalHours = 2, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Cutler, AlternativeWorkWeek = false, TotalHours = 3, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, AlternativeWorkWeek = false, TotalHours = 4, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, AlternativeWorkWeek = false, TotalHours = 5, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, AlternativeWorkWeek = false, TotalHours = 6, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, AlternativeWorkWeek = false, TotalHours = 7, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, AlternativeWorkWeek = false, TotalHours = 8, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, AlternativeWorkWeek = false, TotalHours = 8.01M, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Reedley, AlternativeWorkWeek = false, TotalHours = 9, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Reedley, AlternativeWorkWeek = false, TotalHours = 11.99M, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, AlternativeWorkWeek = false, TotalHours = 12M, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, AlternativeWorkWeek = false, TotalHours = 12.01M, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, AlternativeWorkWeek = false, TotalHours = 13, OverTimeHours = 0, DoubleTimeHours = 0},
+			};
+
+			_plantDailyOTDTHoursCalculator.SetDailyOTDTHours(dailySummaries);
+
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 1 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 2 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 3 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 4 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 5 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 6 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 7 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 8 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 8.01M && x.OverTimeHours == .01M).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 9 && x.OverTimeHours == 1).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 11.99M && x.OverTimeHours == 3.99M).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 12M && x.OverTimeHours == 4M).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 12.01M && x.OverTimeHours == 4M).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 13M && x.OverTimeHours == 4M).Count());
+
+		}
+
+		[TestMethod]
+		public void RegularSchedule_DoubleTimeOverTwelveHours()
+		{
+			var dailySummaries = new List<DailySummary>
+			{
+				new DailySummary{ Crew = (int)Plant.Cutler, AlternativeWorkWeek = false, TotalHours = 12, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, AlternativeWorkWeek = false, TotalHours = 13, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, AlternativeWorkWeek = false, TotalHours = 14, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Reedley, AlternativeWorkWeek = false, TotalHours = 15, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, AlternativeWorkWeek = false, TotalHours = 16.25M, OverTimeHours = 0, DoubleTimeHours = 0},
+			};
+
+			_plantDailyOTDTHoursCalculator.SetDailyOTDTHours(dailySummaries);
+
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 12 && x.DoubleTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 13 && x.DoubleTimeHours == 1).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 14 && x.DoubleTimeHours == 2).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 15 && x.DoubleTimeHours == 3).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 16.25M && x.DoubleTimeHours == 4.25M).Count());
+		}
+
+		[TestMethod]
+		public void AlternativeSchedule_OverTimeBetweenTenAndTwelveHours()
+		{
+			var dailySummaries = new List<DailySummary>
+			{
+				new DailySummary{ Crew = (int)Plant.Cutler, AlternativeWorkWeek = true, TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Cutler, AlternativeWorkWeek = true, TotalHours = 2, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Cutler, AlternativeWorkWeek = true, TotalHours = 3, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, AlternativeWorkWeek = true, TotalHours = 4, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, AlternativeWorkWeek = true, TotalHours = 5, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, AlternativeWorkWeek = true, TotalHours = 6, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, AlternativeWorkWeek = true, TotalHours = 7, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, AlternativeWorkWeek = true, TotalHours = 8, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, AlternativeWorkWeek = true, TotalHours = 8.01M, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, AlternativeWorkWeek = true, TotalHours = 9, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, AlternativeWorkWeek = true, TotalHours = 11.99M, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, AlternativeWorkWeek = true, TotalHours = 12M, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Reedley, AlternativeWorkWeek = true, TotalHours = 12.01M, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Reedley, AlternativeWorkWeek = true, TotalHours = 13, OverTimeHours = 0, DoubleTimeHours = 0},
+			};
+
+			_plantDailyOTDTHoursCalculator.SetDailyOTDTHours(dailySummaries);
+
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 1 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 2 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 3 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 4 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 5 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 6 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 7 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 8 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 8.01M && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 9 && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 11.99M && x.OverTimeHours == 1.99M).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 12M && x.OverTimeHours == 2M).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 12.01M && x.OverTimeHours == 2M).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 13M && x.OverTimeHours == 2M).Count());
+		}
+
+		[TestMethod]
+		public void AlternativeSchedule_DoubleTimeOverTwelveHours()
+		{
+			var dailySummaries = new List<DailySummary>
+			{
+				new DailySummary{ Crew = (int)Plant.Cutler, AlternativeWorkWeek = true, TotalHours = 12, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, AlternativeWorkWeek = true, TotalHours = 13, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, AlternativeWorkWeek = true, TotalHours = 14, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Reedley, AlternativeWorkWeek = true, TotalHours = 15, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, AlternativeWorkWeek = true, TotalHours = 16.25M, OverTimeHours = 0, DoubleTimeHours = 0},
+			};
+
+			_plantDailyOTDTHoursCalculator.SetDailyOTDTHours(dailySummaries);
+
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 12 && x.DoubleTimeHours == 0).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 13 && x.DoubleTimeHours == 1).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 14 && x.DoubleTimeHours == 2).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 15 && x.DoubleTimeHours == 3).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.TotalHours == 16.25M && x.DoubleTimeHours == 4.25M).Count());
+		}
+
+		[TestMethod]
+		public void SeventhDay_OverTimeUpToEightHours()
+		{
+			var dailySummaries = new List<DailySummary>
+			{
+				new DailySummary{ Crew = (int)Plant.Cutler, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 17), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Cutler, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 18), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Cutler, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 19), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 20), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 21), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 22), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 23), TotalHours = 10, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 17), TotalHours = 0, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 18), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 19), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 20), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Reedley, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 21), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Reedley, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 22), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Reedley, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 23), TotalHours = 8, OverTimeHours = 0, DoubleTimeHours = 0}
+			};
+
+			_plantDailyOTDTHoursCalculator.SetDailyOTDTHours(dailySummaries);
+
+			Assert.AreEqual(1, dailySummaries.Where(x => x.EmployeeId == "Employee1" && x.ShiftDate == new DateTime(2020, 2, 23) && x.OverTimeHours == 8).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.EmployeeId == "Employee2" && x.ShiftDate == new DateTime(2020, 2, 23) && x.OverTimeHours == 0).Count());
+		}
+
+		[TestMethod]
+		public void SeventhDay_DoubleTimeOverEightHours()
+		{
+			var dailySummaries = new List<DailySummary>
+			{
+				new DailySummary{ Crew = (int)Plant.Cutler, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 17), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Cutler, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 18), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Cutler, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 19), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 20), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 21), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Kerman, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 22), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 23), TotalHours = 10, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 17), TotalHours = 0, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Office, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 18), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Reedley, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 19), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Reedley, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 20), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 21), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 22), TotalHours = 1, OverTimeHours = 0, DoubleTimeHours = 0},
+				new DailySummary{ Crew = (int)Plant.Sanger, EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), ShiftDate = new DateTime(2020, 2, 23), TotalHours = 10, OverTimeHours = 0, DoubleTimeHours = 0}
+			};
+
+			_plantDailyOTDTHoursCalculator.SetDailyOTDTHours(dailySummaries);
+
+			Assert.AreEqual(1, dailySummaries.Where(x => x.EmployeeId == "Employee1" && x.ShiftDate == new DateTime(2020, 2, 23) && x.DoubleTimeHours == 2).Count());
+			Assert.AreEqual(1, dailySummaries.Where(x => x.EmployeeId == "Employee2" && x.ShiftDate == new DateTime(2020, 2, 23) && x.DoubleTimeHours == 0).Count());
+		}
+	}
+}
