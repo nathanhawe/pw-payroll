3229b486379d592a0b860b5c81a514f88fb49273
Nathan Hawe <nathanhawe@gmail.com>
2020-03-19 12:13:18 -0700

    Complete plant weekly over time calculations. - Implement PlantWeeklyOTHoursCalculator - Update tests

--- Payroll.Service/PlantWeeklyOTHoursCalculator.cs
+++ Payroll.Service/PlantWeeklyOTHoursCalculator.cs
@@ -6,11 +6,46 @@ using System.Text;
 
 namespace Payroll.Service
 {
+	/// <summary>
+	/// Performs weekly over time hours calculations for plant weekly summaries.
+	/// </summary>
 	public class PlantWeeklyOTHoursCalculator : IWeeklyOTHoursCalculator
 	{
+		private decimal WeeklyOverTimeThreshold { get; } = 40M;
+		private readonly IRoundingService _roundingService;
+
+		public PlantWeeklyOTHoursCalculator(IRoundingService roundingService)
+		{
+			_roundingService = roundingService ?? throw new ArgumentNullException(nameof(roundingService));
+		}
+
+		/// <summary>
+		/// Creates new <c>WeeklyOverTimeHours</c> for the provided <c>WeeklySummary</c> objects.  This method 
+		/// expects there is only one <c>WeeklySummary</c> for each employee and week end date.
+		/// </summary>
+		/// <param name="weeklySummaries"></param>
+		/// <returns></returns>
 		public List<WeeklyOverTimeHours> GetWeeklyOTHours(List<WeeklySummary> weeklySummaries)
 		{
-			throw new NotImplementedException();
+			decimal total;
+			var weeklyOverTimeHours = new List<WeeklyOverTimeHours>();
+
+			foreach(var summary in weeklySummaries)
+			{
+				total = _roundingService.Round(summary.TotalHours - summary.TotalOverTimeHours - summary.TotalDoubleTimeHours - WeeklyOverTimeThreshold, 2);
+				if(total > 0)
+				{
+					weeklyOverTimeHours.Add(new WeeklyOverTimeHours
+					{
+						EmployeeId = summary.EmployeeId,
+						Crew = summary.Crew,
+						WeekEndDate = summary.WeekEndDate,
+						OverTimeHours = total
+					});
+				}
+			}
+
+			return weeklyOverTimeHours;
 		}
 	}
 }
--- Payroll.UnitTest/PlantWeeklyOTHoursCalculatorTests.cs
+++ Payroll.UnitTest/PlantWeeklyOTHoursCalculatorTests.cs
@@ -12,7 +12,14 @@ namespace Payroll.UnitTest
 	[TestClass]
 	public class PlantWeeklyOTHoursCalculatorTests
 	{
-		private PlantWeeklyOTHoursCalculator _ranchWeeklyOTHoursCalculator = new PlantWeeklyOTHoursCalculator();
+		private readonly RoundingService _roundingService = new RoundingService();
+		private PlantWeeklyOTHoursCalculator _ranchWeeklyOTHoursCalculator;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_ranchWeeklyOTHoursCalculator = new PlantWeeklyOTHoursCalculator(_roundingService);
+		}
 
 		[TestMethod]
 		public void WeeklyOTAfterFourtyHours()
@@ -20,18 +27,19 @@ namespace Payroll.UnitTest
 			var weeklySummaries = new List<WeeklySummary>
 			{
 				new WeeklySummary{ EmployeeId = "Employee1", Crew = (int)Plant.Cutler, WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, TotalHours = 60, TotalOverTimeHours = 12, TotalDoubleTimeHours = 0},
-				new WeeklySummary{ EmployeeId = "Employee2", Crew = (int)Plant.Kerman, WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, TotalHours = 30, TotalOverTimeHours = 6, TotalDoubleTimeHours = 0},
-				new WeeklySummary{ EmployeeId = "Employee2", Crew = (int)Plant.Office, WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8.5M, TotalHours = 30, TotalOverTimeHours = 6, TotalDoubleTimeHours = 0},
-				new WeeklySummary{ EmployeeId = "Employee3", Crew = (int)Plant.Reedley, WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, TotalHours = 36, TotalOverTimeHours = 12, TotalDoubleTimeHours = 0},
-				new WeeklySummary{ EmployeeId = "Employee3", Crew = (int)Plant.Sanger, WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8.5M, TotalHours = 26, TotalOverTimeHours = 8, TotalDoubleTimeHours = 2}
+				new WeeklySummary{ EmployeeId = "Employee2", Crew = (int)Plant.Office, WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8.5M, TotalHours = 60, TotalOverTimeHours = 0, TotalDoubleTimeHours = 0},
+				new WeeklySummary{ EmployeeId = "Employee3", Crew = (int)Plant.Sanger, WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8.5M, TotalHours = 62, TotalOverTimeHours = 20, TotalDoubleTimeHours = 2},
+				new WeeklySummary{ EmployeeId = "Employee4", Crew = (int)Plant.Kerman, WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8.5M, TotalHours = 70, TotalOverTimeHours = 20, TotalDoubleTimeHours = 2},
+				new WeeklySummary{ EmployeeId = "Employee5", Crew = (int)Plant.Reedley, WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8.5M, TotalHours = 56, TotalOverTimeHours = 8, TotalDoubleTimeHours = 0}
 			};
 
 			var weeklyOverTimeHours = _ranchWeeklyOTHoursCalculator.GetWeeklyOTHours(weeklySummaries);
 
-			Assert.AreEqual(3, weeklyOverTimeHours.Count());
-			Assert.AreEqual(1, weeklyOverTimeHours.Where(x => x.EmployeeId == "Employee1" && x.Crew == 8 && x.WeekEndDate == new DateTime(2020, 2, 23) && x.OverTimeHours == 8).Count());
-			Assert.AreEqual(1, weeklyOverTimeHours.Where(x => x.EmployeeId == "Employee2" && x.Crew == 8 && x.WeekEndDate == new DateTime(2020, 2, 23) && x.OverTimeHours == 8).Count());
-			Assert.AreEqual(1, weeklyOverTimeHours.Where(x => x.EmployeeId == "Employee3" && x.Crew == 8 && x.WeekEndDate == new DateTime(2020, 2, 23) && x.OverTimeHours == 0).Count());
+			Assert.AreEqual(4, weeklyOverTimeHours.Count());
+			Assert.AreEqual(1, weeklyOverTimeHours.Where(x => x.EmployeeId == "Employee1" && x.Crew == (int)Plant.Cutler && x.WeekEndDate == new DateTime(2020, 2, 23) && x.OverTimeHours == 8).Count());
+			Assert.AreEqual(1, weeklyOverTimeHours.Where(x => x.EmployeeId == "Employee2" && x.Crew == (int)Plant.Office && x.WeekEndDate == new DateTime(2020, 2, 23) && x.OverTimeHours == 20).Count());
+			Assert.AreEqual(1, weeklyOverTimeHours.Where(x => x.EmployeeId == "Employee4" && x.Crew == (int)Plant.Kerman && x.WeekEndDate == new DateTime(2020, 2, 23) && x.OverTimeHours == 8).Count());
+			Assert.AreEqual(1, weeklyOverTimeHours.Where(x => x.EmployeeId == "Employee5" && x.Crew == (int)Plant.Reedley && x.WeekEndDate == new DateTime(2020, 2, 23) && x.OverTimeHours == 8).Count());
 		}
 	}
 }
