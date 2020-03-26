f4b3178be7b965f3f19ea542ad6079173e430a74
Nathan Hawe <n.hawe@gerawan.com>
2020-03-26 14:16:20 -0700

    Complete ranch minimum make up calculations.

--- Payroll.Service/RanchMinimumMakeUpCalculator.cs
+++ Payroll.Service/RanchMinimumMakeUpCalculator.cs
@@ -1,15 +1,53 @@
 ﻿using Payroll.Domain;
+using Payroll.Service.Interface;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Payroll.Service
 {
+	/// <summary>
+	/// Performs minimum make up calculations for ranch weekly summaries.
+	/// </summary>
 	public class RanchMinimumMakeUpCalculator
 	{
+		private readonly IRoundingService _roundingService;
+
+		public RanchMinimumMakeUpCalculator(IRoundingService roundingService)
+		{
+			_roundingService = roundingService ?? throw new ArgumentNullException(nameof(roundingService));
+		}
+
+		/// <summary>
+		/// Creates <c>MinimumMakeUp</c>s for the provided <c>WeeklySummary</c> objects.
+		/// </summary>
+		/// <param name="weeklySummaries"></param>
+		/// <returns></returns>
 		public List<MinimumMakeUp> GetMinimumMakeUps(List<WeeklySummary> weeklySummaries)
 		{
-			throw new NotImplementedException();
+			var minimumMakeUps = new List<MinimumMakeUp>();
+			decimal makeUpGross;
+
+			foreach (var summary in weeklySummaries)
+			{
+				if (summary.EffectiveHourlyRate >= summary.MinimumWage) continue;
+
+				makeUpGross = _roundingService.Round((summary.MinimumWage * summary.TotalHours) - summary.TotalGross, 2);
+
+				if (makeUpGross > 0)
+				{
+					minimumMakeUps.Add(new MinimumMakeUp
+					{
+						EmployeeId = summary.EmployeeId,
+						Crew = summary.Crew,
+						WeekEndDate = summary.WeekEndDate,
+						ShiftDate = summary.WeekEndDate,
+						Gross = makeUpGross
+					});
+				}
+			}
+
+			return minimumMakeUps;
 		}
 	}
 }
--- Payroll.UnitTest/RanchMinimumMakeUpCalculatorTests.cs
+++ Payroll.UnitTest/RanchMinimumMakeUpCalculatorTests.cs
@@ -11,7 +11,14 @@ namespace Payroll.UnitTest
 	[TestClass]
 	public class RanchMinimumMakeUpCalculatorTests
 	{
-		private RanchMinimumMakeUpCalculator _minimumMakeUpCalculator = new RanchMinimumMakeUpCalculator();
+		private RoundingService _roundingService = new RoundingService();
+		private RanchMinimumMakeUpCalculator _minimumMakeUpCalculator;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_minimumMakeUpCalculator = new RanchMinimumMakeUpCalculator(_roundingService);
+		}
 
 		[TestMethod]
 		public void EffectiveRateIsLessThanMinimum_GenerateMinimumMakeUps()
@@ -20,15 +27,17 @@ namespace Payroll.UnitTest
 			{
 				new WeeklySummary { EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, EffectiveHourlyRate = 7, TotalGross = 100.03M, TotalHours = 14.29M},
 				new WeeklySummary { EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, EffectiveHourlyRate = 6.5M, TotalGross = 260, TotalHours = 40},
-				new WeeklySummary { EmployeeId = "Employee3", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 15, EffectiveHourlyRate = 14.99M, TotalGross = 599.6M, TotalHours = 40}
+				new WeeklySummary { EmployeeId = "Employee3", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, EffectiveHourlyRate = 6.5M, TotalGross = 130, TotalHours = 20},
+				new WeeklySummary { EmployeeId = "Employee3", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 15, EffectiveHourlyRate = 14.99M, TotalGross = 299.8M, TotalHours = 20}
 			};
 
 			var minimumMakeUps = _minimumMakeUpCalculator.GetMinimumMakeUps(weeklySummaries);
 
-			Assert.AreEqual(3, minimumMakeUps.Count());
-			Assert.AreEqual(1, minimumMakeUps.Where(x => x.EmployeeId == "Employee1" && x.Gross == 14.29M));
-			Assert.AreEqual(1, minimumMakeUps.Where(x => x.EmployeeId == "Employee2" && x.Gross == 60M));
-			Assert.AreEqual(1, minimumMakeUps.Where(x => x.EmployeeId == "Employee3" && x.Gross == .4M));
+			Assert.AreEqual(4, minimumMakeUps.Count());
+			Assert.AreEqual(1, minimumMakeUps.Where(x => x.EmployeeId == "Employee1" && x.Gross == 14.29M).Count());
+			Assert.AreEqual(1, minimumMakeUps.Where(x => x.EmployeeId == "Employee2" && x.Gross == 60M).Count());
+			Assert.AreEqual(1, minimumMakeUps.Where(x => x.EmployeeId == "Employee3" && x.Gross == 30M).Count());
+			Assert.AreEqual(1, minimumMakeUps.Where(x => x.EmployeeId == "Employee3" && x.Gross == .2M).Count());
 		}
 
 		[TestMethod]
@@ -38,7 +47,8 @@ namespace Payroll.UnitTest
 			{
 				new WeeklySummary { EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, EffectiveHourlyRate = 8, TotalGross = 114.32M, TotalHours = 14.29M},
 				new WeeklySummary { EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, EffectiveHourlyRate = 8, TotalGross = 320, TotalHours = 40},
-				new WeeklySummary { EmployeeId = "Employee3", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 15, EffectiveHourlyRate = 15M, TotalGross = 600M, TotalHours = 40}
+				new WeeklySummary { EmployeeId = "Employee3", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, EffectiveHourlyRate = 8M, TotalGross = 160M, TotalHours = 20},
+				new WeeklySummary { EmployeeId = "Employee3", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 15, EffectiveHourlyRate = 15M, TotalGross = 300M, TotalHours = 20}
 			};
 
 			var minimumMakeUps = _minimumMakeUpCalculator.GetMinimumMakeUps(weeklySummaries);
@@ -53,7 +63,8 @@ namespace Payroll.UnitTest
 			{
 				new WeeklySummary { EmployeeId = "Employee1", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, EffectiveHourlyRate = 8, TotalGross = 114.32M, TotalHours = 14.29M},
 				new WeeklySummary { EmployeeId = "Employee2", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, EffectiveHourlyRate = 8, TotalGross = 320, TotalHours = 40},
-				new WeeklySummary { EmployeeId = "Employee3", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 15, EffectiveHourlyRate = 15M, TotalGross = 600M, TotalHours = 40}
+				new WeeklySummary { EmployeeId = "Employee3", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 8, EffectiveHourlyRate = 8M, TotalGross = 160M, TotalHours = 20},
+				new WeeklySummary { EmployeeId = "Employee3", WeekEndDate = new DateTime(2020, 2, 23), MinimumWage = 15, EffectiveHourlyRate = 15M, TotalGross = 300M, TotalHours = 20}
 			};
 
 			var minimumMakeUps = _minimumMakeUpCalculator.GetMinimumMakeUps(weeklySummaries);
