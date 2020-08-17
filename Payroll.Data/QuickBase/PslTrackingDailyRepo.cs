42bf821c9baa2554c3995ab149f7195351999d87
Nathan Hawe <nathanhawe@gmail.com>
2020-08-17 11:31:05 -0700

    Make PSL records unique by location.

--- Payroll.Data/QuickBase/PslTrackingDailyRepo.cs
+++ Payroll.Data/QuickBase/PslTrackingDailyRepo.cs
@@ -54,7 +54,7 @@ namespace Payroll.Data.QuickBase
 			var sb = new StringBuilder();
 			foreach (var line in paidSickLeaves)
 			{
-				sb.Append($"{line.EmployeeId} {line.ShiftDate:M/d/yyyy},");
+				sb.Append($"{line.EmployeeId} {line.ShiftDate:M/d/yyyy} {line.Company},");
 				sb.Append($"{line.EmployeeId},");
 				sb.Append($"{line.ShiftDate:MM-dd-yyyy},");
 				sb.Append($"{line.Company},");
